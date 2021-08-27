2a45ed266d187114558872ce69b8cded348b297c
vinayakyengandul <vinayakyengandul@gmail.com>
2021-08-27 15:31:08 +0530

    remove unwanted class

--- lib/greytrix_ui_kit.dart
+++ lib/greytrix_ui_kit.dart
@@ -6,9 +6,3 @@ export './src/widgets/index.dart';
 export 'package:flutter_signature_pad/flutter_signature_pad.dart';
 export './src/controllers/index.dart';
 export 'package:flex_color_scheme/flex_color_scheme.dart';
-
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
